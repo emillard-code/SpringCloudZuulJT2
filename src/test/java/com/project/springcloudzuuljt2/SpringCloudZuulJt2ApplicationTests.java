08e1cc18346c83d9cec7f4480b20f2679e9a8236
Emerson Millard <emillard.code@gmail.com>
2022-07-23 12:00:00 -0700

    Modified test class.

--- src/test/java/com/project/springcloudzuuljt2/SpringCloudZuulJt2ApplicationTests.java
+++ src/test/java/com/project/springcloudzuuljt2/SpringCloudZuulJt2ApplicationTests.java
@@ -1,13 +1,8 @@
 package com.project.springcloudzuuljt2;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringCloudZuulJt2ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
