0b3271ada2987641a8b10ea6452a4f38f23a4690
Emerson Millard <emillard.code@gmail.com>
2022-07-23 12:00:00 -0700

    Added controller method to main class.

--- src/main/java/com/project/springcloudzuuljt2/SpringCloudZuulJt2Application.java
+++ src/main/java/com/project/springcloudzuuljt2/SpringCloudZuulJt2Application.java
@@ -3,6 +3,7 @@ package com.project.springcloudzuuljt2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,6 +14,13 @@ public class SpringCloudZuulJt2Application {
     public String getDoctorService() {
 
         return "Doctor microservice called...";
+
+    }
+
+    @GetMapping("/welcome/{name}")
+    public String wish(@PathVariable String name) {
+
+        return "Hi "+name+" Welcome to Doctor Service";
 
     }
 
