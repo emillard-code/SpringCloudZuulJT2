632572195493fba962f9b3800eb65697695a0553
Emerson Millard <emillard.code@gmail.com>
2022-07-24 12:00:00 -0700

    Moved controller methods to controller class.

--- src/main/java/com/project/SpringCloudZuulJt2Application.java
+++ src/main/java/com/project/SpringCloudZuulJt2Application.java
@@ -2,27 +2,9 @@ package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringCloudZuulJt2Application {
-
-    @GetMapping("/getDoctor")
-    public String getDoctorService() {
-
-        return "Doctor microservice called...";
-
-    }
-
-    @GetMapping("/welcome/{name}")
-    public String wish(@PathVariable String name) {
-
-        return "Hi "+ name +" Welcome to Doctor Service";
-
-    }
 
     public static void main(String[] args) {
 
