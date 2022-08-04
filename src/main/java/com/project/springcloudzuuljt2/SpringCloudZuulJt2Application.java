a2e135f12969f3e39f1523b686a796ec442b5277
Emerson Millard <emillard.code@gmail.com>
2022-08-04 09:42:08 -0700

    Initial commit

---
+++ src/main/java/com/project/springcloudzuuljt2/SpringCloudZuulJt2Application.java
@@ -0,0 +1,13 @@
+package com.project.springcloudzuuljt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudZuulJt2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudZuulJt2Application.class, args);
+    }
+
+}
